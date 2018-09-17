645798de3e826483aa95d0e71652f237bcf7eade
Nozomi Matsuoka <n.m.romie@4s.local>
2018-09-17 16:04:42 +0800

    Rev-03 User: Lesson&Result func

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,7 +1,7 @@
 class Word < ApplicationRecord
 	belongs_to :category, optional: true
 	has_many :choices, dependent: :destroy
-  has_many :answers
+  has_many :answers, dependent: :destroy
 	
 	accepts_nested_attributes_for :choices
 
