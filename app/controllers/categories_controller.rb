10e33c05f0065304fa700c5a1556a821de15e953
Nozomi Matsuoka <n.m.romie@4s.local>
2018-09-27 09:02:04 +0800

    Temp: User's Lesson sort function

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,36 @@ class CategoriesController < ApplicationController
 	before_action :require_login, only: [:index]
 
 	def index
-		@categories = Category.paginate(page: params[:page], per_page: 6)
+
+		@categories = Category.all
+
+		@ready = [nil]
+		@learned_lesson = [nil]
+		@learn_lesson = [nil]
+
+		@categories.each do |category|
+			if category.words.empty?
+				@ready << category
+			elsif current_user.lessons.pluck(:category_id).any?(category.id)
+				@learned_lesson << category
+			else
+				@learn_lesson << category
+			end
+		end
+
+		if params[:learned] == 1
+			@learned = 1				
+			@categories = @ready.compact
+			redirect_to categories_path(learned: 1)
+		elsif params[:learned] == 2
+			@learned = 2
+			@categories = @learned_lesson.compact
+			redirect_to categories_path(learned: 2)
+		elsif params[:learned] == 3
+			@learned = 3			
+			@categories = @learn_lesson.compact
+			redirect_to categories_path(learned: 3)
+		end
 		@lesson = current_user.lessons
 	end
 
